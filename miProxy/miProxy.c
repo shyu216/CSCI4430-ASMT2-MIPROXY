36a9bf71dc45b82e6ad7567aea36754c425a48aa
shyu216 <1155141630@link.cuhk.edu.hk>
2022-03-03 16:00:47 +0800

    update

--- miProxy/miProxy.c
+++ miProxy/miProxy.c
@@ -7,8 +7,43 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/time.h>
+
 #include "DNSHeader.h"
 #include "DNSQuestion.h"
 #include "DNSRecord.h"
 
-#include <time.h>
+#include "miHandler.h"
+#include "miCalculator.h"
+#include "miChooser.h"
+#include "miLogger.h"
+
+int handler(int listen_port, char *www_ip, double alpha, char *log = argv[5])
+{
+    
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 6 && strcmp(argv[1], "--nodns") == 0)
+    {
+        int listen_port = atoi(argv[2]);
+        char *www_ip = argv[3];
+        double alpha = atof(argv[4]);
+        char *log = argv[5];
+
+        return handler(listen_port, www_ip, alpha, log);
+    }
+    else if (argc == 7 && strcmp(argv[1], "--nodns") == 0)
+    {
+        // bonus part here.
+        printf("We dont have bonus part.\n");
+        return 1;
+    }
+    else
+    {
+        printf("Error: missing or extra arguments\n");
+        return 1;
+    }
+}
